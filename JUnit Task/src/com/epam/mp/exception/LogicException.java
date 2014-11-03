1560116c1cbf33a680ad07184cf6685083dd6027
Anastasiya <lilnastenka@gmail.com>
2014-11-03 20:59:53 +0300

    Deleted old version of Junit task from repository
    Deleted old version of Junit task from repository

--- JUnit Task/src/com/epam/mp/exception/LogicException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.mp.exception;
-
-public class LogicException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public LogicException(String message) {
-		super(message);
-	}
-}
